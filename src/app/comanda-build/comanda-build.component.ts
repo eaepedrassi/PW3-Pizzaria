e67f12a6296324e09367564aa4ffee60095cd445
Aluno <Aluno@LI03-18>
2019-10-04 20:33:25 -0300

    Exercicio 11 parte 1

---
+++ src/app/comanda-build/comanda-build.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-comanda-build',
+  templateUrl: './comanda-build.component.html',
+  styleUrls: ['./comanda-build.component.css']
+})
+export class ComandaBuildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
